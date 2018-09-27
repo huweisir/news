9a7ba3c5c059a8de7c85c905a5a9dd3cd7d88fd8
wb.huwei@mesg.corp.netease.com <1654301540@qq.com>
2018-09-27 18:07:10 +0800

    change:sh变量去除

--- forever.sh
+++ forever.sh
@@ -1,6 +1,7 @@
 #!/bin/sh  
 #! forever run 
-tsnode="node -r ts-node/register "
+#! tsnode="node -r ts-node/register "
+git pull --rebase
 forever stopall
-forever start -c "${tsnode}" src/index.ts
+orever start -c "node -r ts-node/register" src/index.ts
 
